387ac05110e73426ef59dbc56068fde4662b564d
Avinash Kokare <avi1999kokare@gmail.com>
2022-02-16 22:19:14 +0530

    Changed return messege for hello_world

--- ocr_utility/ocr_api/views.py
+++ ocr_utility/ocr_api/views.py
@@ -2,12 +2,14 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.conf.urls import url, include
 
-@api_view(['GET', 'POST'])
+
+@api_view(["GET", "POST"])
 def hello_world(request):
-        if request.method == 'POST':
-                return Response({"message": "Got some data!", "data": request.data})
-        return Response({"message": "Hello, world!"})
+    if request.method == "POST":
+        return Response({"message": "Got some data!", "data": request.data})
+    return Response({"message": "Hello, world! This is ocr_utility"})
+
 
 urlpatterns = [
-  url(r'hello', hello_world),
+    url(r"hello", hello_world),
 ]
