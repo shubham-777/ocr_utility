85a83d98aa03e7f580a08b919bf6511342d4efea
Parth Mangtani <impmmmm@gmail.com>
2022-02-17 22:36:47 +0530

    edit second route to accept files

--- ocr_utility/ocr_api/views.py
+++ ocr_utility/ocr_api/views.py
@@ -1,6 +1,8 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from django.conf.urls import url, include
+from rest_framework.views import APIView
+from rest_framework.parsers import FileUploadParser
+from django.conf.urls import url
 
 
 @api_view(["GET", "POST"])
@@ -10,12 +12,18 @@ def hello_world(request):
     return Response({"message": "Hello, world! This is ocr_utility"})
 
 
-@api_view(["POST"])
-def input_ocr(request):
-    return Response({"your_ocr": request.data})
+class FileUploadView(APIView):
+    parser_classes = [FileUploadParser]
+
+    def post(self, request):
+        file_obj = request.data['file']
+        # ...
+        # do some stuff with uploaded file
+        # ...
+        return Response(status=204)
 
 
 urlpatterns = [
     url(r"hello", hello_world),
-    url(r"input_ocr", input_ocr),
+    url(r"input_ocr", FileUploadView.as_view()),
 ]
